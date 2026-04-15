eccace38f9cb64f1f3596190cd058baa0b8ce983
FernandaHolandaBarreto <fernanda.barreto@edu.senai.br>
2026-04-14 21:21:14 -0300

    Update sa1.js

--- sa1.js
+++ sa1.js
@@ -5,50 +5,55 @@ let meta = 10000
 
 //O programa deverá solicitar os seguintes dados//
 
-let nome = readline.question("Digite o nome do funcionário")
-let cargo =readline.question("Digite o cargo")
-let salarioBase = readline.questionFloat("Digite o salário base")
-let resultado = readline.questionFloat("Digite o resultado alcançado (R$):")
+let nome = readline.question("Digite o nome do funcionário: ")
+let cargo =readline.question("Digite o cargo: ")
+let salarioBase = readline.questionFloat("Digite o salário base: ")
+let resultado = readline.questionFloat("Digite o resultado alcançado (R$): ")
 
 //Calculo do desempenho//
 
 let desempenho = (resultado / meta )*100
 
-//Vereficação da meta//
+//Verificação da meta//
 
 let falta = 0
 let percentualFalta = 0
 
-let = (resultado < meta)
- falta = meta - resultado
+if (resultado < meta){
+   falta = meta - resultado
+   percentualFalta = (falta / meta) * 100
+}
 
 //Classificação e bônus//
 
-let classificação
+let classificacao
 let bonus = 0
 
-if (desempenho < 70){
-    console.log("Classifição insuficente")
+if (desempenho < 70) {
+    classificacao = "Insuficiente"
+    bonus = 0
+} else if (desempenho < 100) {
+    classificacao = "Satisfatório"
+    bonus = salarioBase * 0.05
 } else {
-    console.log("Classifição satisfatório")
-    bonus = salarioBase *0.05
-
-    console.log("Classificação Excelente")
-    bonus = salarioBase *0.10
+    classificacao = "Excelente"
+    bonus = salarioBase * 0.10
 }
 
 console.log("=========Resultado=======")
 
 let salarioFinal = salarioBase + bonus
 
-console.log("Nome")
-console.log("Cargo")
-console.log("Desempenho fixo(2) + %")
-console.log("Faltou fixo (2)")
-console.log("Classificação")
-console.log("Bônus fixo")
-console.log("Salário Final  salarioFinal fixo(2)")
-
+console.log("Nome:", nome)
+console.log("Cargo", cargo)
+console.log("Desempenho:", desempenho.toFixed(2) + "%")
+console.log("Classificação:", classificacao)
+console.log("Bônus: R$", bonus.toFixed(2))
+console.log("Salário Final: R$", salarioFinal.toFixed(2))
+if (resultado < meta) {
+    console.log("Faltou: R$", falta.toFixed(2))
+    console.log("Percentual que faltou:", percentualFalta.toFixed(2) + "%")
+}
 
 
 
